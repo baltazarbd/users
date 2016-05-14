ed70612ac1fc13ea837ef9c7368868011f0a128b
root <baltazarbd@gmail.com>
2016-05-14 20:28:04 +0300

    new version

--- host.sh
+++ host.sh
@@ -1,41 +1,45 @@
 #!/bin/bash 
 IFS=$'\n'
 
-trap "rm -f /tmp/all-hosts1 /tmp/all-hosts2 /tmp/alive-hosts /tmp/all-hosts" 0 1 2 5 15  
+trap "rm -f /tmp/all-hosts1 /tmp/all-hosts2 /tmp/alive-hosts /tmp/all-hosts /tmp/ip-and-hostname" 0 1 2 5 15  
 
 #find all hosts
-nmap -p22 -n --host-timeout 30 172.24.30.0/24 | sed "s/Nmap.*for/***/g" | grep -v "STATE" | grep -v "Host is" | grep -A 1 "\*\*\*" | tr -d "\n" | sed -e "s/22\/tcp//g" -e "s/ssh/ssh\n/g" > /tmp/all-hosts1
+nmap -p22 -n --host-timeout 30 172.24.0.0/16 | sed "s/Nmap.*for/***/g" | grep -v "STATE" | grep -v "Host is" | grep -A 1 "\*\*\*" | tr -d "\n" | sed -e "s/22\/tcp//g" -e "s/ssh/ssh\n/g" > /tmp/all-hosts1
+
 #find v2
-nmap -n -sP 172.24.30.* | grep "report for" | awk '{print $(NF)}' > /tmp/alive-hosts
+nmap -n -sP 172.24.*.* | grep "report for" | awk '{print $(NF)}' > /tmp/alive-hosts
 cat /tmp/alive-hosts | xargs nmap -n -p22 |  sed "s/Nmap.*for/***/g" | grep -v "STATE" | grep -v "Host is" | grep -A 1 "\*\*\*" | tr -d "\n" | sed -e "s/22\/tcp//g" -e "s/ssh/ssh\n/g" > /tmp/all-hosts2
+
 #combine
 cat /tmp/all-hosts1 /tmp/all-hosts2 | sort | uniq > /tmp/all-hosts
 
 #distr detect
 
-for i in `cat /tmp/all-hosts | grep open | awk '{ print $2}'`; do 
-    echo -n " "$i" "; timeout 10 ssh -o StrictHostKeyChecking=no -q -o UserKnownHostsFile=/dev/null -o "BatchMode yes" $i hostname ; echo ""; 
+for i in `cat /tmp/all-hosts | grep open | awk '{ print $2}'| sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n`; do 
+    echo -n " "$i" "; timeout 10 ssh -o StrictHostKeyChecking=no -q -o UserKnownHostsFile=/dev/null -o "BatchMode yes" $i hostname; 
 done  > /tmp/ip-and-hostname
-
-
-
 
 for ip in `cat /tmp/ip-and-hostname`; do
     ip_for_sqlite=`echo $ip| cut -f 2 -d " "`
     host_for_sqlite=`echo $ip| cut -f 3 -d " "`
-    ip_in_db=`sqlite3 users.db "select ip from  hosts;"  |  grep -c $ip `;
+    only_ip=`echo $ip |awk '{print $1}'`
+    sqlite_ip=`echo \'$only_ip\'`
+    ip_in_db=`sqlite3 users.db "select ip from  hosts where ip = $sqlite_ip;" |grep -c $only_ip`;
+    
     if [ "$ip_in_db" = "0" ]; then
-    sqlite3 users.db "insert into hosts (ip,hostname) values ('$ip_for_sqlite','$host_for_sqlite');"
+	sqlite3 users.db "insert into hosts (ip,hostname) values ('$ip_for_sqlite','$host_for_sqlite');"
     fi
 done
 
+for del_old_ip in `sqlite3 users.db "select ip from  hosts;"`; do 
+    current_ip=`cat /tmp/all-hosts |grep -c $del_old_ip`
+    
+    if [ "$current_ip" = "0" ]; then
+	sqlite3 users.db "delete from  hosts where ip= '$del_old_ip'"
+    fi
 
-#for ip in `iptables-save |grep WHITELIST |cut -f 4 -d " " | sed "s/\/32//g"  | grep -v tcp`; do
-#    NGINXIP=`cat /etc/nginx/access/* /etc/nginx/conf.d/iplist-access.include |grep -v '#'  |  grep -c $i `;
-#    if [ "$NGINXIP" = "0" ]; then
-#    iptables -D $IPCHAIN -s $i $IPTABLCMD
-#    fi
-#done
+done
+
 
 
 
--- test.0.1.sh
+++ test.0.1.sh
@@ -1,14 +1,20 @@
 #! /bin/sh
 
 #Global
-user_key_list=`ls /root/ |grep key |awk -F. '{print $1  " User  off " }'|sort`
-server_list=`cat /root/serverlist|awk  '{print $1,$2, "off " }'|sort`
+user_key_list=`sqlite3 users.db "select login from users;" |awk '{print $1  " User  off " }'|sort`
+server_list=`sqlite3 users.db "select * from  hosts"|awk  '{print $1,$2"off" }'|sed 's/|/ /'`
+
+
 #Dialog
 DIALOG=${DIALOG=dialog}  #переменая для вызова диалога 
 tempfile=`mktemp 2>/dev/null` || tempfile=/tmp/test$$  #тмп файлик для диалога
 trap "rm -f $tempfile" 0 1 2 5 15  #очищаем после себя врменный файлик если получили завершающий сигнал 0 1 2 5 15 
 
 #Функции 
+
+#######################################################################################
+#######################################################################################
+
 #######  Выбираем юзера 
 dialog_choose_user () {
     $DIALOG --backtitle "Choose user" \
@@ -47,7 +53,7 @@ dialog_choose_group () {
 dialog_choose_server () {
     $DIALOG --backtitle "Choose servers" \
 	--title "Servers list " --clear \
-	--checklist "Choose servers:" 30 60 30  ${server_list}       2> $tempfile
+	--checklist "Choose servers:" 50 80 50  ${server_list}       2> $tempfile
     retval=$?
     choice_server=`cat $tempfile`
     case $retval in
@@ -58,10 +64,10 @@ dialog_choose_server () {
     esac
 }
 
-##	Добовляем ключик 
+##	Добовляем ключик на сервер  
 add_ssh_key () {
 	    if [ "$check_sshdir_in_homedir_on_server" = "1" ]; then
-		usersshkey=`cat /root/$choice_user.key`
+		usersshkey=`cat /root/users/$choice_user.key`
 		ssh $server_ip "echo $usersshkey > /home/$choice_user/.ssh/authorized_keys"
 		ssh  $server_ip "chown $choice_user:$choice_user /home/$choice_user/.ssh/authorized_keys"
 		echo "Add key $choice_user  on $server_ip"
@@ -74,8 +80,46 @@ add_ssh_key () {
 	    fi 
 }
 
+#######################################################################################
+#######################################################################################
+#Добовление ключей  и пользователей в базу 
+
+dialog_enter_username () {
+    $DIALOG --title "Ввод данных" --clear \
+	--inputbox "login:" 16 51 2> $tempfile
+    retval=$?
+    login_for_sqlite=`cat $tempfile`
+    case $retval in
+	1)
+	    ./$0 ;;
+	255)
+	    echo "Нажата клавиша ESC."
+	;;
+    esac
+}
 
 
+dialog_enter_sshkey () {
+
+    $DIALOG --title "Ввод данных" --clear \
+	--inputbox "ssh key:" 16 51 2> $tempfile
+    retval=$?
+    sshkey_for_sqlite=`cat $tempfile`
+    case $retval in
+	1)
+	    ./$0 ;;
+	255)
+	    echo "Нажата клавиша ESC."
+	;;
+    esac
+}
+
+
+
+
+
+#######################################################################################
+#######################################################################################
 
 #Main screen 
 $DIALOG --backtitle "Choose action" \
@@ -86,9 +130,9 @@ $DIALOG --backtitle "Choose action" \
 	    "3" "Обновить ssh ключ" off \
 	    "4" "Заблокировать пользователя" off \
 	    "5" "Разблокировать пользователя" off \
-	    "6" "Посмотреть судо права для группы " off \
-	    "7" "Дать права судо группе " off \
-	    "8" "Удалить права судо  для группы " off   2> $tempfile
+	    "6" "Добавить пользователя в базу  " off \
+	    "7" "Удалить из базы" off \
+	    "8" "Обновить ключ в базе " off   2> $tempfile
 
     retval=$?
     choice_action=`cat $tempfile`
@@ -158,7 +202,7 @@ case $choice_action in
 
 	for server_ip in ${choice_server} ; do
 	check_sshdir_in_homedir_on_server=`ssh  $server_ip ls -la /home/$choice_user/ |grep -c ".ssh" `
-	add_ssh_key  
+	add_ssh_key
 	done
     ;;
 
@@ -167,7 +211,7 @@ case $choice_action in
 	dialog_choose_server
 
 	for server_ip in ${choice_server} ; do
-		ssh $server_ip "usermod -L $choice_user "
+		ssh $server_ip "usermod -L $choice_user "                                
 		ssh  $server_ip "rm /home/$choice_user/.ssh/authorized_keys"
 		echo " User $choice_user is block "
 	done
@@ -183,6 +227,22 @@ case $choice_action in
 		echo " User $choice_user is unblock "
 	done
     ;;
+
+    6)
+	dialog_enter_username
+	dialog_enter_sshkey
+
+	login_in_db=`sqlite3 users.db "select * from users;"|grep -c $login_for_sqlite`
+	if [ "$login_in_db" = "0" ]; then
+	    sqlite3 users.db "insert into users (login,ssh_key) values ('$login_for_sqlite','$sshkey_for_sqlite');"
+	fi
+    ;;
+
+    7)
+	dialog_choose_user
+	sqlite3 users.db "delete from users where login='$choice_user';"
+    ;;
+
 
 
 esac 
